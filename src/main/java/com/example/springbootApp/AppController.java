45432ec349c3f79a5a97c8b52aa8a9846d92110c
arjun-526 <nagarjuna.anke3@gmail.com>
2023-01-06 00:50:40 +0530

    added controller class

---
+++ src/main/java/com/example/springbootApp/AppController.java
@@ -0,0 +1,15 @@
+package com.example.springbootApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Hello World";
+	}
+	
+	
+}
